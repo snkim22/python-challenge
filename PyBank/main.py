c1feb315b5f52b6b9f04b19b3a7c46446afa552d
snkim22 <sarah.kim@bpmatt.com>
2023-03-22 22:49:52 -0700

    removed unnecessary comments

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,10 +1,3 @@
-
-# The total number of months included in the dataset
-# The net total amount of "Profit/Losses" over the entire period
-# The changes in "Profit/Losses" over the entire period, and then the average of those changes
-# The greatest increase in profits (date and amount) over the entire period
-# The greatest decrease in profits (date and amount) over the entire period
-
 import csv
 
 # define file path 
